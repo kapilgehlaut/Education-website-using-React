1c2688d0538ebc8aec2ff2d223448dd37cae8ed3
Kapil Gehlaut <kapilgehlaut17@gmail.com>
2024-05-02 13:34:45 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css"
 import Header from "./components/common/header/Header"
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {Route} from 'react-router-dom';
 import About from "./components/about/About"
 import CourseHome from "./components/allcourses/CourseHome"
 import Team from "./components/team/Team"
@@ -12,9 +12,7 @@ import Home from "./components/home/Home"
 function App() {
   return (
     <>
-      <Router>
         <Header />
-        <Switch>
           <Route exact path='/' component={Home} />
           <Route exact path='/about' component={About} />
           <Route exact path='/courses' component={CourseHome} />
@@ -22,9 +20,7 @@ function App() {
           <Route exact path='/pricing' component={Pricing} />
           <Route exact path='/journal' component={Blog} />
           <Route exact path='/contact' component={Contact} />
-        </Switch>
         <Footer />
-      </Router>
     </>
   )
 }
